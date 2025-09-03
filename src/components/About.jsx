6d2ca1cd64038c4429dc20f074fbfcd6f440db87
nikhilmekala09 <24P31F0056@acet.ac.in>
2025-09-03 14:20:56 +0530

    my first change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ const About = () => {
   return (
     <section id="about" className="py-10 px-4 max-w-4xl mx-auto">
       <h2 className="text-2xl font-bold mb-4">About Me</h2>
-      <p>I’m Sirisha, a DevOps/Cloud Engineer passionate about automating infrastructure, building CI/CD pipelines, and deploying scalable applications on Azure and AWS.</p>
+      <p>I’m Nikhil, a DevOps/Cloud Engineer passionate about automating infrastructure, building CI/CD pipelines, and deploying scalable applications on Azure and AWS.</p>
     </section>
   );
 };
